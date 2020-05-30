41a0e3b72d8a07ab3e780dd707577ebead09d87d
NUC8 <nuc8@NUC8s-Mac-mini.lan>
2020-05-30 15:29:24 +0800

    Add element-ui

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const headerTag = [{
+  text: '全部',
+  className: 'all'
+}, {
+  text: '精华',
+  className: 'good'
+}, {
+  text: '分享',
+  className: 'share'
+}, {
+  text: '问答',
+  className: 'ask'
+}, {
+  text: '招聘',
+  className: 'job'
+}, {
+  text: '测试',
+  className: 'dev'
+}]
